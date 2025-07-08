9d45443d9b270cc076cebf5fa4455e724118ca84
manthan684 <post.manthan@gmail.com>
2025-07-08 16:52:31 +0530

    Create rono 1234.js

---
+++ rono 1234.js
@@ -0,0 +1,26 @@
+ //1. Multiply Two Numbers//
+ function multiply(a, b) {
+  return a * b;
+}
+
+console.log(multiply(3, 4)); // Output: 12
+
+
+//2. Get the First Character of a String//
+
+function firstCharacter(str) {
+  return str[0];
+}
+
+console.log(firstCharacter("hello")); // Output: "h"
+
+
+
+//3. Check if a Number is Positive//
+
+function isPositive(num) {
+  return num > 0;
+}
+
+console.log(isPositive(5));   // Output: true
+console.log(isPositive(-3));  // Output: false
